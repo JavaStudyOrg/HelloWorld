d908bde20844c4860d738d3ed0399ed650d1dd7e
CaduNote <CaduNote@CaduNote-PC>
2015-03-04 23:08:27 -0300

    Teste do cadu

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,8 @@ import controllers.IndexController;
 public class HelloWorld {
 	public static void main(String[] args) {
 		IndexController index = new IndexController();
-		
 		index.printHello();
-		
 		index.printAtt();
+		System.out.println("deu");
 	}
 }
